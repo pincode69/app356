31e5fe2467fdd839b0330ca6dd52cd9c163daee6
pincode69 <pin.a.s.o.aso@gmail.com>
2026-08-07 01:01:09 +0300

    fix name of app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import AppBootstrap from './AppBootstrap';
-// import { MASTER_ANDROID } from './MASTER_ANDROID';
 
 function App() {
   return <AppBootstrap useRootNavigator />;
-  // return <MASTER_ANDROID />
 }
 
 export default App;
